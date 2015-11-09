cf0f298d8f079a8b6d874be00ca25217b79d8f7c
kennwhite <kenneth.white@doveltech.com>
2015-11-08 20:57:12 -0500

    Adding new py27-letsencrypt package by Kubilay Kocak

--- scripts/FreeBSD10.sh
+++ scripts/FreeBSD10.sh
@@ -3,24 +3,31 @@
 # FreeBSD 10.2-RELEASE stock build (on AWS: ami-f709a29c) LetsEncrypt beta client installer
 #
 # If you get stuck, just try again:
-#  cd ~/ && rm -rf ./letsencrypt && rm -rf ~/.local/share/letsencrypt && rm -rf /etc/letsencrypt && rm -rf ~/.cache/pip
+#  cd ~/ && rm -rf ./letsencrypt && rm -rf ~/.local/share/letsencrypt && rm -rf /usr/local/etc/letsencrypt && rm -rf ~/.cache/pip
 #
-# Usage (as root; note the leading space): . ./FreeBSD10.sh
+
+# Usage (as root; note the leading space): source ./FreeBSD10.sh
 
 # Set this to your domain for certificate
-
 export DOMAIN=www.EXAMPLE.org
 
-su - root -c 'pkg install -y git sudo'
 
-# If on AWS, add ec2-user to sudoers if not present (yes, it's a hack)
-if ( ping -c 1 `hostname`| grep -q ec2.internal ) then
-  grep 'ec2' /usr/local/etc/sudoers || su - root -c 'echo "ec2-user ALL=(ALL) NOPASSWD: ALL" >> /usr/local/etc/sudoers'
-fi
+mkdir -p /usr/local/etc/pkg/repos
 
-git clone https://github.com/letsencrypt/letsencrypt
-cd letsencrypt
-./letsencrypt-auto --agree-dev-preview -d $DOMAIN --authenticator manual certonly
+# Currently only available for 10.2-Release
+
+cat > /usr/local/etc/pkg/repos/FreeBSD.conf << 'EOF'
+ FreeBSD: {
+  url: "pkg+http://pkg.FreeBSD.org/${ABI}/latest"
+ }
+'EOF'
+
+pkg install -y py27-letsencrypt
+
+letsencrypt --agree-dev-preview -d $DOMAIN --authenticator manual certonly
+
 
 # For trusted cert (currently Beta testers only):
-#  --server https://acme-v01.api.letsencrypt.org/directory
+#  --server https://acme-v01.api.letsencrypt.org/directory 
+
+
